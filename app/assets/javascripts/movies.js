2434e6beed66979fae10177a6dd9dc7b5a2f7c9d
Jon <jon_p_stone@yahoo.com>
2017-12-25 09:59:01 -0600

    Finished off movie previews on writer page

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -4,10 +4,17 @@ $(function(){
       var $ul = $("div.movies ul")
       $ul.html("")
       json.forEach(function(movie){
-        var length = 350;
+        var title = movie.title;
         var narrative = movie.content;
-        var trunc = narrative.substring(0, length);
-        $ul.append("<li>" + trunc + "..." + "</li><br>");
+        var trunc = narrative.substring(0, 350);
+        var writer = movie.writer_id
+        var review = movie.id
+        var linkText = "Read more";
+        var url = writer + "/reviews/" + review
+        $ul.append(
+          "<li>" + "<h4 class='title'>" + title + "</h4>" +
+          trunc + "..." + "<strong>" + linkText.link(url) + "</strong></li><br>"
+        );
       })
     })
     e.preventDefault();
