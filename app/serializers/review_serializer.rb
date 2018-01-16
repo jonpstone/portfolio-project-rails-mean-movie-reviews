a7a930945508cc67367a1620fbcaefeb668d8da2
Jon <jon_p_stone@yahoo.com>
2018-01-16 03:54:22 -0600

    Solved AMS issue interfering with AJAX call on writers show page, updated spec also.

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :content, :title, :image, :writer_id, :year, :date_published
-  belongs_to :genre, serializer: ReviewGenreSerializer
+  belongs_to :genre, serializer: ReviewGenreSerializer, if: -> { false }
 end
