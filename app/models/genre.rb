1f4594bf36738c291c870e571d756a1ba43de46e
Jonathan Peter Stone <github email address>
2018-02-20 13:43:58 -0600

    Added delete action to genre form partial.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,7 @@ class Genre < ApplicationRecord
   has_many :review_genres
   has_many :reviews, through: :review_genres
 
+  validates_uniqueness_of :genre_name
   validates_presence_of :genre_name
   validates :genre_name, length: { minimum: 3 }
 end
