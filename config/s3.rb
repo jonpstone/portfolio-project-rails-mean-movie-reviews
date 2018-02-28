a377c9a33041cc71f8ea56f893746473f7b13953
Jonathan Peter Stone <github email address>
2018-02-28 17:05:25 -0600

    Corrected region in AWS s3 config file.

--- config/s3.rb
+++ config/s3.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => "AKIAI4S2ZHRPFQNQECIQ",
       :aws_secret_access_key  => "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",
-      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
+      :region                 => 'us-east-2' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "mean-review-assets"
 end
