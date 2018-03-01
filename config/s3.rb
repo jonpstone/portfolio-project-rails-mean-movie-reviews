75d451666cd7e6226fa0ce77d9788ba9e2f4ac14
Jonathan Peter Stone <github email address>
2018-02-28 18:11:10 -0600

    Using fog-aws gem as alternative.

--- config/s3.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAI4S2ZHRPFQNQECIQ",
-      :aws_secret_access_key  => "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",
-      :region                 => 'us-east-1'
-  config.fog_directory  = "mean-review-assets"
-end
