25f25ded5c82422f01aa03b6aa6e8a78effb6fec
Jonathan Peter Stone <github email address>
2018-03-21 11:50:51 -0500

    Minor rearrangements to js code.

--- app/assets/javascripts/previews.js
+++ app/assets/javascripts/previews.js
@@ -24,10 +24,19 @@ $(function(){
         var readMore = "Read more";
 
         $ul.append(
-          `<li style='display: inline-block; vertical-align: top;'><a href='${url}'>
-          <img src='${movie.image.url}' width='100' height='140' style='float: left; padding-right: 20px; padding-bottom: 20px'></a>
-          <a href='${url}'><h4 class='title' style='margin-top: -0.05%;'>${movie.title} | ${movie.year}</h4></a>
-          <p style='margin-top: 2%;'>${trunc}...<strong>${readMore.link(url)}</strong></p></li>`
+          `<li style='display: inline-block; vertical-align: top;'>
+          <a href='${url}'><img src='${movie.image.url}'
+            width='100'
+            height='140'
+            style='float: left; padding-right: 20px; padding-bottom: 20px'>
+          </a>
+          <a href='${url}'>
+          <h4 class='title' style='margin-top: -0.05%;'>
+            ${movie.title} | ${movie.year}
+          </h4></a>
+          <p style='margin-top: 2%;'>
+            ${trunc}...<strong>${readMore.link(url)}</strong>
+          </p></li>`
         );
       });
     });
